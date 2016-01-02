6e5055fa7e36c3da90dd031259d67f7a479e947a
= <=>
2016-01-02 23:49:25 +0800

    DBConntction.py

--- utils/DBConnection.py
+++ utils/DBConnection.py
@@ -1,5 +1,18 @@
 #!/usr/bin/env python2.7
 # -*- coding:utf-8 -*-
 
+import redis
+
 __author__ = 'zhuzw'
 
+
+class RedisConnection:
+
+    def __init__(self, dbargs):
+        self.connect = redis.Redis(host=dbargs['host'], port=dbargs['post'],db=0)
+        self.pipe = self.connect.pipeline()
+
+    def exp_grid(self, grid):
+        _prefix = 'GRID_'
+        key = _prefix + grid
+        return self.connect.hvals(key)
