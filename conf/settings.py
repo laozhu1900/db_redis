53825cd9f0dbda5d7c6a9bad50a43d50f280df28
= <=>
2016-01-04 10:00:41 +0800

    modify conf.settings

--- conf/settings.py
+++ conf/settings.py
@@ -2,3 +2,10 @@
 # -*- coding:utf-8 -*-
 
 __author__ = 'zhuzw'
+
+db_args = {
+
+    "host": "127.0.0.1",
+    "port": "6379"
+
+}
--- utils/DBConnection.py
+++ utils/DBConnection.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python2.7
 # -*- coding:utf-8 -*-
 
+# from conf import setting
 import redis
 
 __author__ = 'zhuzw'
@@ -8,11 +9,9 @@ __author__ = 'zhuzw'
 
 class RedisConnection:
 
-    def __init__(self, dbargs):
-        self.connect = redis.Redis(host=dbargs['host'], port=dbargs['post'],db=0)
+    def __init__(self, db_args):
+        self.connect = redis.Redis(host=db_args['host'], port=db_args['post'], db=0)
         self.pipe = self.connect.pipeline()
 
-    def exp_grid(self, grid):
-        _prefix = 'GRID_'
-        key = _prefix + grid
-        return self.connect.hvals(key)
+
+
